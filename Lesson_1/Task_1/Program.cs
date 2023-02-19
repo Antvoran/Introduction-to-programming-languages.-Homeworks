ba6c6bd547f27f5d2608c9c7e74c91d15fb7715e
Антон <Antvoran@yandex.by>
2023-02-20 01:11:10 +0300

    Завершили первую задачу Task_1

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,4 +1,14 @@
 ﻿// Написать программу, которая на входе принимает два числа, а на выходе определяет, 
 // какое из них больше, а какое меньше.
 
-
+Console.WriteLine("Введите первое целое число:");
+String firstNum = Console.ReadLine();
+int firstNumber = Convert.ToInt32(firstNum);
+Console.WriteLine("Введите второе целое число:");
+String secondNum = Console.ReadLine();
+int secondNumber = Convert.ToInt32(secondNum);
+if (firstNumber > secondNumber) {
+    Console.WriteLine($"Первое число {firstNumber} больше, чем второе число {secondNumber}.");
+}
+else
+Console.WriteLine($"Второе число {secondNumber} больше, чем первое число {firstNumber}.");
