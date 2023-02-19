1a5725aa9bc6b546350feba2971b19130470d992
Антон <Antvoran@yandex.by>
2023-02-20 02:48:09 +0300

    Создали программу для четвёртой задачи Task_4

--- Lesson_1/Task_4/Program.cs
+++ Lesson_1/Task_4/Program.cs
@@ -1,4 +1,12 @@
 ﻿// Написать программу, которая на входе принимает число N, 
 // а на выходе выдаёт все чётные числа от 1 до N.
 
-
+Console.WriteLine("Введите целое положительное число:");
+String Num = Console.ReadLine();
+int Number = Convert.ToInt32(Num);
+int i = 2;
+Console.WriteLine($"Чётные числа от 1 до {Number}:");
+while (i < Number + 1){
+    Console.Write($"{i}, ");
+    i = i + 2;
+    }
