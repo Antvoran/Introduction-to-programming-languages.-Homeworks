612684fc28617892669118e9ab1067b5953a6059
Антон <Antvoran@yandex.by>
2023-02-20 02:15:16 +0300

    Создали программу для третьей задачи Task_3

--- Lesson_1/Task_3/Program.cs
+++ Lesson_1/Task_3/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Написать программу, которая на входе принимает число, 
 // а на выходе определяет, является ли оно чётным.
 
+Console.WriteLine("Введите целое число:");
+String Num = Console.ReadLine();
+int Number = Convert.ToInt32(Num);
+if (Number % 2 == 0){
+    Console.WriteLine($"Число {Number} - чётное.");
+}
+else
+Console.WriteLine($"Число {Number} - нечётное.");
