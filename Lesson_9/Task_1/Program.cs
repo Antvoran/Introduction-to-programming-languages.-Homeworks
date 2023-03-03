e7ecdeed943576369b84fc81ef7e50b8242b9f8f
Антон <Antvoran@yandex.by>
2023-03-03 19:07:23 +0300

    Создал папку Lesson_9 но она почему то не добавляется на гитхаб

---
+++ Lesson_9/Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные 
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение M:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Чётные числа в промежутке от " + m + " до " + n + ":");
+
+        PrintEvenNumbers(m, n);
+    }
+
+    static void PrintEvenNumbers(int start, int end)
+    {
+        if (start > end)
+        {
+            return;
+        }
+
+        if (start % 2 == 0)
+        {
+            Console.WriteLine(start);
+        }
+
+        PrintEvenNumbers(start + 1, end);
+    }
+}
