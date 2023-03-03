8b12c169ec11007c236d3fde5d6eb0d98cb3e3a5
Антон <Antvoran@yandex.by>
2023-03-03 19:31:56 +0300

    Создали папку Lesson_9/Task_2 с решением второй задачи

---
+++ Lesson_9/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// чисел в промежутке от M до N.
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = m; i <= n; i++) {
+            sum += i;
+        }
+
+        Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {sum}");
+    }
+}
